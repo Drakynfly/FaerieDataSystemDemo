0966ae65325842fb06ac92de84d34788708624ee
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2024-11-23 16:16:41 -0800

    mostly fix the grid

--- Source/FDS_DEV/Private/Spatial/UI/DemoSpatialGrid.cpp
+++ Source/FDS_DEV/Private/Spatial/UI/DemoSpatialGrid.cpp
@@ -1,22 +1,30 @@
 // Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
 
-
 #include "Spatial/UI/DemoSpatialGrid.h"
-
 #include "Spatial/UI/DemoProxyHolder.h"
-#include "InventoryStorageProxy.h"
-#include "Blueprint/WidgetTree.h"
-#include "Components/CanvasPanelSlot.h"
-#include "Components/PanelWidget.h"
-#include "Components/UniformGridSlot.h"
 #include "Spatial/UI/DemoDraggableBase.h"
 #include "Spatial/UI/DemoSpatialDDO.h"
 
-UWidget* UDemoSpatialGrid::GetChildUnderCursor(const FVector2D AbsolutePosition, UPanelWidget* InPanel)
+#include "Blueprint/WidgetTree.h"
+#include "Components/GridSlot.h"
+
+UWidget* UDemoSpatialGrid::GetCellUnderCursor(const FVector2D AbsolutePosition) const
 {
-	for (const auto& Child : InPanel->GetAllChildren())
+	for (auto&& Child : CellWidgets)
 	{
-		if (Child->GetCachedGeometry().IsUnderLocation(AbsolutePosition))
+		if (IsValid(Child) && Child->GetCachedGeometry().IsUnderLocation(AbsolutePosition))
+		{
+			return Child;
+		}
+	}
+	return nullptr;
+}
+
+UWidget* UDemoSpatialGrid::GetItemUnderCursor(const FVector2D AbsolutePosition) const
+{
+	for (auto&& Child : IconWidgets)
+	{
+		if (IsValid(Child) && Child->GetCachedGeometry().IsUnderLocation(AbsolutePosition))
 		{
 			return Child;
 		}
@@ -25,73 +33,66 @@ UWidget* UDemoSpatialGrid::GetChildUnderCursor(const FVector2D AbsolutePosition,
 }
 
 void UDemoSpatialGrid::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent,
-	UDragDropOperation*& OutOperation)
+											UDragDropOperation*& OutOperation)
 {
 	UDemoSpatialDDO* OutDDO = NewObject<UDemoSpatialDDO>();
 	//OutDDO->Pivot = EDragPivot::MouseDown;
 	OutOperation = OutDDO;
 	const FVector2D ScreenSpacePosition = InMouseEvent.GetScreenSpacePosition();
-    
-	// Iterate through children of GridPanel
-	for (auto& Child : GridPanel->GetAllChildren())
+
+	if (auto&& CellWidget = GetCellUnderCursor(ScreenSpacePosition))
 	{
-		if (Child->GetCachedGeometry().IsUnderLocation(ScreenSpacePosition))
+		if (const auto* GridSlot = Cast<UGridSlot>(CellWidget->Slot))
 		{
-			if (const auto* GridSlot = Cast<UUniformGridSlot>(Child->Slot))
-			{
-				OutDDO->SourcePoint = FIntPoint(GridSlot->GetColumn(), GridSlot->GetRow());
-				OutDDO->Payload = Child;
-			}
-			break;
+			OutDDO->SourcePoint = FIntPoint(GridSlot->GetColumn(), GridSlot->GetRow());
+			OutDDO->Payload = CellWidget;
 		}
 	}
-	
-	for (const auto& Child : IconPanel->GetAllChildren())
-	{
-		if (Child->GetCachedGeometry().IsUnderLocation(ScreenSpacePosition))
-		{
-			auto DragWidget = CreateWidget<UDemoDraggableBase>(GetOwningPlayer(), UDemoDraggableBase::StaticClass());
-			if (const auto* DraggableBase = Cast<UUserWidget>(Child))
-			{
-				// Get the original widget's absolute size in screen space
-				const FVector2D OriginalScreenSize = Child->GetCachedGeometry().GetAbsoluteSize();
-            
-				// Get the original widget's desired size
-				const FVector2D OriginalDesiredSize = Child->GetDesiredSize();
-            
-				// Calculate the current scale factor being applied by the ScaleBox
-				const FVector2D CurrentScale = OriginalScreenSize / OriginalDesiredSize;
-				// Get widgets from the Child widget (DraggableBase) instead of IconPanel
-				const auto TargetIcon = Cast<UImage>(DraggableBase->WidgetTree->FindWidget(TEXT("Icon")));
-				const auto TargetIconSize = Cast<USizeBox>(DraggableBase->WidgetTree->FindWidget(TEXT("IconSize")));
-               
-				if (TargetIcon && TargetIconSize)
-				{
-					DragWidget->RemoveFromParent();
-					DragWidget->DragIcon->SetBrush(TargetIcon->GetBrush());
-					DragWidget->DragIconSize->SetWidthOverride(TargetIconSize->GetWidthOverride());
-					DragWidget->DragIconSize->SetHeightOverride(TargetIconSize->GetHeightOverride());
-             
-					// Calculate the offset between click and icon position
-					const FVector2D MouseScreenPos = InMouseEvent.GetScreenSpacePosition();
-					const FVector2D IconScreenPos = Child->GetCachedGeometry().GetAbsolutePosition();
-					const FVector2D ClickOffset = MouseScreenPos - IconScreenPos;
-					DragWidget->SetPositionInViewport(MouseScreenPos);
 
-					// Apply position with the offset 
-					const FVector2D AdjustedPos = MouseScreenPos - ClickOffset;
-					DragWidget->SetRenderScale(CurrentScale - 1);
-					DragWidget->SetRenderTranslation(AdjustedPos);
-             
-					OutOperation->DefaultDragVisual = DragWidget;
-					OutOperation->Pivot = EDragPivot::MouseDown;
-					if (const auto* ProxyHolder = Cast<UDemoProxyHolder>(Child))
-					{
-						OutDDO->Proxy = ProxyHolder->Proxy;
-					}
+	if (auto&& ItemWidget = GetItemUnderCursor(ScreenSpacePosition))
+	{
+		auto DragWidget = CreateWidget<UDemoDraggableBase>(GetOwningPlayer(), UDemoDraggableBase::StaticClass());
+		if (const auto* DraggableBase = Cast<UUserWidget>(ItemWidget))
+		{
+			// Get the original widget's absolute size in screen space
+			const FVector2D OriginalScreenSize = ItemWidget->GetCachedGeometry().GetAbsoluteSize();
+
+			// Get the original widget's desired size
+			const FVector2D OriginalDesiredSize = ItemWidget->GetDesiredSize();
+
+			// Calculate the current scale factor being applied by the ScaleBox
+			const FVector2D CurrentScale = OriginalScreenSize / OriginalDesiredSize;
+			// Get widgets from the Child widget (DraggableBase) instead of
+
+			// @todo why
+			const auto TargetIcon = Cast<UImage>(DraggableBase->WidgetTree->FindWidget(TEXT("Icon")));
+			const auto TargetIconSize = Cast<USizeBox>(DraggableBase->WidgetTree->FindWidget(TEXT("IconSize")));
+
+			if (TargetIcon && TargetIconSize)
+			{
+				DragWidget->RemoveFromParent();
+				DragWidget->DragIcon->SetBrush(TargetIcon->GetBrush());
+				DragWidget->DragIconSize->SetWidthOverride(TargetIconSize->GetWidthOverride());
+				DragWidget->DragIconSize->SetHeightOverride(TargetIconSize->GetHeightOverride());
+
+				// Calculate the offset between click and icon position
+				const FVector2D MouseScreenPos = InMouseEvent.GetScreenSpacePosition();
+				const FVector2D IconScreenPos = ItemWidget->GetCachedGeometry().GetAbsolutePosition();
+				const FVector2D ClickOffset = MouseScreenPos - IconScreenPos;
+				DragWidget->SetPositionInViewport(MouseScreenPos);
+
+				// Apply position with the offset
+				const FVector2D AdjustedPos = MouseScreenPos - ClickOffset;
+				DragWidget->SetRenderScale(CurrentScale - 1);
+				DragWidget->SetRenderTranslation(AdjustedPos);
+
+				OutOperation->DefaultDragVisual = DragWidget;
+				OutOperation->Pivot = EDragPivot::MouseDown;
+				if (const auto* ProxyHolder = Cast<UDemoProxyHolder>(ItemWidget))
+				{
+					OutDDO->Proxy = ProxyHolder->Proxy;
 				}
 			}
-			break;
 		}
 	}
 	Super::NativeOnDragDetected(InGeometry, InMouseEvent, OutOperation);
--- Source/FDS_DEV/Public/Spatial/UI/DemoSpatialGrid.h
+++ Source/FDS_DEV/Public/Spatial/UI/DemoSpatialGrid.h
@@ -16,13 +16,16 @@ class FDS_DEV_API UDemoSpatialGrid : public UUserWidget
 public:
 	virtual void NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation) override;
 
-	UFUNCTION(BlueprintCallable)
-	UWidget* GetChildUnderCursor(const FVector2D AbsolutePosition, UPanelWidget* InPanel);
+	UFUNCTION(BlueprintCallable, Category = "Spatial Grid")
+	UWidget* GetCellUnderCursor(const FVector2D AbsolutePosition) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Spatial Grid")
+	UWidget* GetItemUnderCursor(const FVector2D AbsolutePosition) const;
 
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
-	TObjectPtr<UPanelWidget> GridPanel;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TObjectPtr<UWidget>> CellWidgets;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(BindWidget))
-	TObjectPtr<UPanelWidget> IconPanel;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TObjectPtr<UWidget>> IconWidgets;
 };
